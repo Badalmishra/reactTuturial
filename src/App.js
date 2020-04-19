f29d90c3d4618df1c42eb808f8b3b49432c44e73
RajaShahab <badalmishradev@gmail.com>
2020-04-19 11:52:31 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Body from './components/Body';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      username : "abc"
+    }
+  }
+  changeName() {
+    this.setState({username:'new name'})
+  }
+  render(){
+    return (
+      <div>
+        <Header name={this.state.username}/>
+        <hr/>
+        <Body changeName={() => this.changeName()} name={this.state.username}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
